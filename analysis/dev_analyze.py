623fb3d9a4e63a424c58ac0973c1925bdc2e6484
Jian Wei Tay <jian.tay@vai.org>
2026-07-17 17:23:34 -0400

    Adding file merging

--- analysis/dev_analyze.py
+++ analysis/dev_analyze.py
@@ -1,23 +1,56 @@
 # Import an ROI and look for spots
 
+from pathlib import Path
+
 import oic_toolkit
 import skimage
 from matplotlib import pyplot as plt
 
-img_1 = skimage.io.imread(
-    r"../processed/shading_corrected/AM1c-s11-r002_Plate_4555/AM1c-s11-r002_A01_channel1.ome.tif"
-)
+data_dir = Path("../processed/shading_corrected/")
 
-img_3 = skimage.io.imread(
-    r"../processed/shading_corrected/AM1c-s11-r002_Plate_4555/AM1c-s11-r002_A01_channel3.ome.tif"
-)
+ROI = [5340, 172, 11950, 5870]
+
+dataset1_dir = Path("AW GVB AM1c-s11 010426_Plate_4536")
+dataset1_fn = "AW GVB AM1c-s11 010426_A01_channel"
+
+dataset2_dir = Path("AM1c-s11-r002_Plate_4555")
+dataset2_fn = "AM1c-s11-r002_A01_channel"
+
+# Register the images
+img1_dapi = skimage.io.imread(data_dir / dataset1_dir / (dataset1_fn + "2.tif"))
+img1_dapi = img1_dapi[ROI[1] : ROI[3], ROI[0] : ROI[2]]
+
+img2_dapi = skimage.io.imread(data_dir / dataset2_dir / (dataset2_fn + "2.tif"))
+img2_dapi = img2_dapi[ROI[1] : ROI[3], ROI[0] : ROI[2]]
+
+shift, img2_dapi_corr = oic_toolkit.register.phasexcorr(img1_dapi, img2_dapi)
+
+merge = oic_toolkit.display.merge_images(img1_dapi, img2_dapi_corr)
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+axes[0].imshow(img1_dapi)
+axes[1].imshow(img2_dapi)
+axes[2].imshow(merge)
+
+plt.show()
+
+exit()
+
+# img_1 = skimage.io.imread(
+#     r"../processed/shading_corrected/AW GVB AM1c-s11 010426_Plate_4536/AW GVB AM1c-s11 010426_A01_channel1.tif"
+# )
+
+# img_3 = skimage.io.imread(
+#     r"../processed/shading_corrected/AW GVB AM1c-s11 010426_Plate_4536/AW GVB AM1c-s11 010426_A01_channel3.tif"
+# )
 
 
-img_4 = skimage.io.imread(
-    r"../processed/shading_corrected/AM1c-s11-r002_Plate_4555/AM1c-s11-r002_A01_channel4.ome.tif"
-)
+# img_4 = skimage.io.imread(
+#     r"../processed/shading_corrected/AW GVB AM1c-s11 010426_Plate_4536/AW GVB AM1c-s11 010426_A01_channel4.tif"
+# )
 
-ROI = [6200, 700, 7600, 2000]
+# ROI = [6200, 700, 7600, 2000]
+ROI = [8712, 2287, 10000, 3300]
 
 img_1 = img_1[ROI[1] : ROI[3], ROI[0] : ROI[2]]
 img_3 = img_3[ROI[1] : ROI[3], ROI[0] : ROI[2]]
--- tests/test_calculate_shading.py
+++ tests/test_calculate_shading.py
@@ -7,8 +7,14 @@ from shared import gvb_analyzer as ga
 # plt.imshow(shading)
 # plt.show()
 
+# ga.correct_shading(
+#     r"D:\Projects\henderson-lab-brain-gvb\data\Dataset 1\raw\AM1c-s11-r002_Plate_4555\TimePoint_1",
+#     r"D:\Projects\henderson-lab-brain-gvb\processed\20260707_AM1c-s11-r002_Plate_4555",
+#     file_pattern="*w2.TIF",
+# )
+
 ga.correct_shading(
-    r"D:\Projects\henderson-lab-brain-gvb\data\Dataset 1\raw\AM1c-s11-r002_Plate_4555\TimePoint_1",
-    r"D:\Projects\henderson-lab-brain-gvb\processed\20260707_AM1c-s11-r002_Plate_4555",
+    r"D:\Projects\henderson-lab-brain-gvb\data\Dataset 1\raw\AM1c-s11-r003_Plate_4642\TimePoint_1",
+    r"D:\Projects\henderson-lab-brain-gvb\processed\AM1c-s11-r003_Plate_4642",
     file_pattern="*w2.TIF",
 )
