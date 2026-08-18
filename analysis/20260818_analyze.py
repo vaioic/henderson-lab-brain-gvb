93993d9dbc801192d6eb13cd4fe481596212a78f
Jian Wei Tay <jian.tay@vai.org>
2026-08-18 18:21:32 -0400

    WIP: Start integrating cell segmentation and spot detection

--- analysis/20260818_analyze.py
+++ analysis/20260818_analyze.py
@@ -1,6 +1,35 @@
 # Load the cellpose mask
+import skimage
+from matplotlib import pyplot as plt
 
-# Apply the green mask to determine the positive cells
+cell_mask = skimage.io.imread("../processed/2026-08-17 Dev/cell_masks.tif")
+
+# Read the green channel
+psyn_image = skimage.io.imread(
+    r"../test/warped_dataset_output_4555_2/warped_AM1c-s11-r002_A01_channel0.tif"
+)
+
+# Measure the pSyn signal
+cell_props_psyn = skimage.measure.regionprops_table(
+    cell_mask, psyn_image, properties=("intensity_mean", "label", "coords")
+)
+
+final_cell_mask = cell_mask.copy()
+
+for idx in range(len(cell_props_psyn["label"])):
+    if cell_props_psyn["intensity_mean"][idx] <= 500:
+        coords = cell_props_psyn["coords"][idx]
+
+        final_cell_mask[coords[:, 0], coords[:, 1]] = False
+
+plt.imshow(final_cell_mask)
+plt.show()
+
+exit()
+
+plt.imshow(psyn_image)
+plt.show()
+
 
 # Look for spots
 
